822635d72958f70607fc2dcc99ce5b550f933792
Abdullah Basit <abdullah.basit@hotmail.com>
2022-08-11 14:10:50 +0500

    Removed type in constructor of PJSIPShowEndpointAction.php

--- src/PAMI/Message/Action/PJSIPShowEndpointAction.php
+++ src/PAMI/Message/Action/PJSIPShowEndpointAction.php
@@ -4,7 +4,7 @@ namespace PAMI\Message\Action;
 
 class PJSIPShowEndpointAction extends ActionMessage
 {
-    public function __construct(string $endpoint)
+    public function __construct($endpoint)
     {
         parent::__construct("PJSIPShowEndpoint");
         $this->setKey('Endpoint', $endpoint);
